aa9f50a78dcf905525fd7248a0d6ab779ac5df9e
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2024-04-25 16:47:05 +0300

    Update authOperations.js

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+//import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { toastError, toastSuccess } from '../helpers/toastcase';
+//import { toastError, toastSuccess } from '../helpers/toastcase';
 
-const insrance = axios.create({
-  baseURL: 'https://movies-site-api.onrender.com',
-  timeout: 1000,
-  headers: { Authorization: '' },
-});
+// const insrance = axios.create({
+//   baseURL: 'https://movies-site-api.onrender.com',
+//   timeout: 1000,
+//   headers: { Authorization: '' },
+// });
 
 //axios.defaults.baseURL = 'https://movies-site-api.onrender.com';
 
