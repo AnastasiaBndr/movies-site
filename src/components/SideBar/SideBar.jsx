6458921e9512d269f70cd2881211834ca3cffdc2
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2024-05-28 23:02:20 +0300

    dasd

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,7 +3,7 @@ import { selectUser, selectIsLoggedIn } from '../../redux/auth/authSelectors';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogOutBtn from '../LogOutBtn/LogOutBtn';
-import './styles.css'
+import './styles.css';
 import { useEffect, useState, useRef } from 'react';
 
 const SideBar = ({ className }) => {
@@ -11,23 +11,26 @@ const SideBar = ({ className }) => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
 
     const [isOpen, setIsOpen] = useState(false);
-    const [sidebarTop, setSidebarTop] = useState(0);
     const headerRef = useRef(null);
 
-    const updateSidebarTop = () => {
+    const updateSidebarPosition = () => {
         const headerHeight = headerRef.current ? headerRef.current.offsetHeight : 0;
         const scrollY = window.scrollY;
-        const newTop = Math.max(headerHeight - scrollY, 0);
-        setSidebarTop(newTop);
+
+        if (scrollY >= headerHeight) {
+            document.querySelector('.side-bar').style.top = `${scrollY}px`;
+        } else {
+            document.querySelector('.side-bar').style.top = `${headerHeight}px`;
+        }
     };
 
     useEffect(() => {
-        window.addEventListener('scroll', updateSidebarTop);
-        return () => window.removeEventListener('scroll', updateSidebarTop);
+        window.addEventListener('scroll', updateSidebarPosition);
+        return () => window.removeEventListener('scroll', updateSidebarPosition);
     }, []);
 
     useEffect(() => {
-        updateSidebarTop(); // Встановлюємо початкове значення top
+        updateSidebarPosition(); // Встановлюємо початкове значення
     }, []);
 
     useEffect(() => {
@@ -46,9 +49,13 @@ const SideBar = ({ className }) => {
         const handleGesture = () => {
             if (touchEndX - touchStartX > 50) {
                 setIsOpen(true);
+                document.body.classList.add('lock-scroll');
+                document.querySelector('.overlay').classList.add('show');
             }
             if (touchStartX - touchEndX > 50) {
                 setIsOpen(false);
+                document.body.classList.remove('lock-scroll');
+                document.querySelector('.overlay').classList.remove('show');
             }
         };
 
@@ -62,45 +69,48 @@ const SideBar = ({ className }) => {
     }, []);
 
     return (
-        <div className={`${className} ${isOpen ? 'open' : ''}`} style={{ top: `${sidebarTop}px` }}>
-            <MainPart>
-                <TopPart>
-                    <ProfilePicContainer>
-                        <UserAvatar fill="#494949">
-                            <use
-                                xlinkHref={
-                                    process.env.PUBLIC_URL +
-                                    '/images/sprite.svg#icon-avatar'
-                                }
-                            ></use>
-                        </UserAvatar>
-                    </ProfilePicContainer>
-                    {isLoggedIn && <p>{currentUser.name}</p>}
-                    {!isLoggedIn && <p>Here may be your name ;)</p>}
-                    {isLoggedIn && <p>{currentUser.username}</p>}
-                </TopPart>
-                <BottomPart>
-                    <NavBar>
-                        <NavLink className="nav-element" to="/">
-                            Home
-                        </NavLink>
-                        {isLoggedIn && currentUser && <NavLink className="nav-element" to={`/current/user/${currentUser.username}`}>
-                            Profile
-                        </NavLink>}
-                        {!isLoggedIn && !currentUser && <NavLink className="nav-element" to={`/login`}>
-                            Profile
-                        </NavLink>}
-                    </NavBar>
-                    <NavBar>
-                        <NavLink className="nav-element" to="/">Favorite</NavLink>
-                        <NavLink className="nav-element" to="/">Dropped</NavLink>
-                        <NavLink className="nav-element" to="/">Watching</NavLink>
-                    </NavBar>
-                </BottomPart>
-                <LogOutBtn>LogOut</LogOutBtn>
-            </MainPart>
-        </div>
+        <>
+            <div className="overlay"></div>
+            <div className={`${className} ${isOpen ? 'open' : ''}`}>
+                <MainPart>
+                    <TopPart>
+                        <ProfilePicContainer>
+                            <UserAvatar fill="#494949">
+                                <use
+                                    xlinkHref={
+                                        process.env.PUBLIC_URL +
+                                        '/images/sprite.svg#icon-avatar'
+                                    }
+                                ></use>
+                            </UserAvatar>
+                        </ProfilePicContainer>
+                        {isLoggedIn && <p>{currentUser.name}</p>}
+                        {!isLoggedIn && <p>Here may be your name ;)</p>}
+                        {isLoggedIn && <p>{currentUser.username}</p>}
+                    </TopPart>
+                    <BottomPart>
+                        <NavBar>
+                            <NavLink className="nav-element" to="/">
+                                Home
+                            </NavLink>
+                            {isLoggedIn && currentUser && <NavLink className="nav-element" to={`/current/user/${currentUser.username}`}>
+                                Profile
+                            </NavLink>}
+                            {!isLoggedIn && !currentUser && <NavLink className="nav-element" to={`/login`}>
+                                Profile
+                            </NavLink>}
+                        </NavBar>
+                        <NavBar>
+                            <NavLink className="nav-element" to="/">Favorite</NavLink>
+                            <NavLink className="nav-element" to="/">Dropped</NavLink>
+                            <NavLink className="nav-element" to="/">Watching</NavLink>
+                        </NavBar>
+                    </BottomPart>
+                    <LogOutBtn>LogOut</LogOutBtn>
+                </MainPart>
+            </div>
+        </>
     );
-}
+};
 
 export default SideBar;
