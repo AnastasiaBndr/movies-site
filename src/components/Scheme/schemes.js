dc09df316d8342fcb562333386c2a8d499518629
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2024-05-30 18:41:41 +0300

    Update schemes.js

--- src/components/Scheme/schemes.js
+++ src/components/Scheme/schemes.js
@@ -15,46 +15,10 @@ import { selectLanguage } from '../../redux/global/globalSlice';
 const MoviesListScheme = ({ movies, location }) => {
   const language = useSelector(selectLanguage) || 'en-US';
   return (
-    <MoviesList>
-      {language === 'en-US'
-        ? movies.map(movie => {
-            var mediaType = null;
-            if (movie.media_type === undefined) {
-              mediaType = 'movie';
-            }
-            return (
-              <MoviesItem key={movie.id || movie.globalId}>
-                <Link
-                  to={
-                    '/' +
-                    (movie.media_type === undefined
-                      ? mediaType
-                      : movie.media_type) +
-                    '/' +
-                    (movie.id || movie.globalId)
-                  }
-                  state={{ from: location }}
-                >
-                  <ImageContainer>
-                    <img
-                      src={`https://image.tmdb.org/t/p/w200${
-                        movie.poster_path || movie.poster
-                      }?api_key=${process.env.KEY}`}
-                      alt={`${movie.title}`}
-                    />
-                    <h3>{movie.title || movie.name}</h3>
-                  </ImageContainer>
-                </Link>
-              </MoviesItem>
-            );
-          })
-        : movies
-            .filter(
-              movie =>
-                movie.poster_path &&
-                movie.overview.includes('а' || 'і' || 'ф' || 'н' || 'п' || 'к')
-            )
-            .map(movie => {
+    movies && (
+      <MoviesList>
+        {language === 'en-US'
+          ? movies.map(movie => {
               var mediaType = null;
               if (movie.media_type === undefined) {
                 mediaType = 'movie';
@@ -84,8 +48,48 @@ const MoviesListScheme = ({ movies, location }) => {
                   </Link>
                 </MoviesItem>
               );
-            })}
-    </MoviesList>
+            })
+          : movies
+              .filter(
+                movie =>
+                  movie.poster_path &&
+                  movie.overview.includes(
+                    'а' || 'і' || 'ф' || 'н' || 'п' || 'к'
+                  )
+              )
+              .map(movie => {
+                var mediaType = null;
+                if (movie.media_type === undefined) {
+                  mediaType = 'movie';
+                }
+                return (
+                  <MoviesItem key={movie.id || movie.globalId}>
+                    <Link
+                      to={
+                        '/' +
+                        (movie.media_type === undefined
+                          ? mediaType
+                          : movie.media_type) +
+                        '/' +
+                        (movie.id || movie.globalId)
+                      }
+                      state={{ from: location }}
+                    >
+                      <ImageContainer>
+                        <img
+                          src={`https://image.tmdb.org/t/p/w200${
+                            movie.poster_path || movie.poster
+                          }?api_key=${process.env.KEY}`}
+                          alt={`${movie.title}`}
+                        />
+                        <h3>{movie.title || movie.name}</h3>
+                      </ImageContainer>
+                    </Link>
+                  </MoviesItem>
+                );
+              })}
+      </MoviesList>
+    )
   );
 };
 
@@ -138,32 +142,34 @@ const PaginationsButtons = ({
 
 const UserFilteresListScheme = ({ movies, onDelete }) => {
   return (
-    <MoviesList>
-      {movies.map(movie => {
-        const mediaType =
-          movie.media_type === undefined ? 'movie' : movie.media_type;
-        return (
-          <MoviesItem key={movie.id || movie.globalId}>
-            <DeleteButton onClick={() => onDelete(movie)}>
-              <SvgDelete viewBox="0 0 32 32">
-                <path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path>
-              </SvgDelete>
-            </DeleteButton>
-            <Link to={`/${mediaType}/${movie.id || movie.globalId}`}>
-              <ImageContainer>
-                <img
-                  src={`https://image.tmdb.org/t/p/w200${
-                    movie.poster_path || movie.poster
-                  }?api_key=${process.env.KEY}`}
-                  alt={`${movie.title}`}
-                />
-                <h3>{movie.title || movie.name}</h3>
-              </ImageContainer>
-            </Link>
-          </MoviesItem>
-        );
-      })}
-    </MoviesList>
+    movies && (
+      <MoviesList>
+        {movies.map(movie => {
+          const mediaType =
+            movie.media_type === undefined ? 'movie' : movie.media_type;
+          return (
+            <MoviesItem key={movie.id || movie.globalId}>
+              <DeleteButton onClick={() => onDelete(movie)}>
+                <SvgDelete viewBox="0 0 32 32">
+                  <path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path>
+                </SvgDelete>
+              </DeleteButton>
+              <Link to={`/${mediaType}/${movie.id || movie.globalId}`}>
+                <ImageContainer>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200${
+                      movie.poster_path || movie.poster
+                    }?api_key=${process.env.KEY}`}
+                    alt={`${movie.title}`}
+                  />
+                  <h3>{movie.title || movie.name}</h3>
+                </ImageContainer>
+              </Link>
+            </MoviesItem>
+          );
+        })}
+      </MoviesList>
+    )
   );
 };
 
